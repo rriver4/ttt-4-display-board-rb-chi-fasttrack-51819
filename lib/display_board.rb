2dd5c108c0cb617500a37ded51390f26e56b0b9d
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 02:34:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board
+  puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
